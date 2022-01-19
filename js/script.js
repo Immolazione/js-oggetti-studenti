4730892ce4adbacecf1494c97c49f599387a8724
Immolazione <federico.deered@gmail.com>
2022-01-19 16:53:45 +0100

    try to print listaStudenti

--- js/script.js
+++ js/script.js
@@ -46,5 +46,11 @@ console.log(alunni);
 // ! 4 CICLO SU TUTTI GLI STUDENTI E STAMPO
 for (i = 0; i < alunni.length; i++) {
     const datiStudente = alunni[i];
-    listaStudenti.innerHTML += `${datiStudente['nome']}, ${datiStudente['cognome']}, ${datiStudente['età']} | `;
-}
+    // listaStudenti.innerHTML += `${datiStudente['nome']}, ${datiStudente['cognome']}, ${datiStudente['età']} | `;
+
+    const alunnoPerRiga = document.createElement('p');
+    alunnoPerRiga.appendChild;
+    alunnoPerRiga.innerHTML = `${datiStudente['nome']}, ${datiStudente['cognome']}, ${datiStudente['età']}`;
+    listaStudenti.innerHTML = alunnoPerRiga;
+    console.log(alunnoPerRiga);
+};
